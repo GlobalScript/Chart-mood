d4bbecfc5abd770df1643c7cffc01bf47d4f071c
GlobalScript <sandr.onlooker@gmail.com>
2022-02-04 15:20:07 +0200

    First 1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.container');
 
 const headText = document.createElement('h1');
-	headText.textContent = "Hello world";
+	headText.textContent = "Hello my friend";
 container.append(headText);
